a6cfb6e49fb1fcb47fea1c11baa82f5e7d35e15f
Łukasz Brzózko <lukasz9099@gmail.com>
2023-08-14 15:32:52 +0200

    config: add tampermonkey meta and user files

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Białystok kamery
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds a simple panel to switch a camera
 // @author       Łukasz Brzózko
 // @match        http://kamery.szr.bialystok.pl:81/*
@@ -9,6 +9,8 @@
 // @resource     openProps https://unpkg.com/open-props@1.5.11/open-props.min.css
 // @resource     openPropsNormalize https://unpkg.com/open-props@1.5.11/normalize.min.css
 // @resource     openPropsButtons https://unpkg.com/open-props@1.5.11/buttons.min.css
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/bialystok-kamery/main/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/bialystok-kamery/main/index.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
