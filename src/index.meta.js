6700501e0c83574b9645739c0126bac2032f26f3
Łukasz Brzózko <lukasz9099@gmail.com>
2023-08-14 17:26:11 +0200

    config: limit target link to kam

--- src/index.meta.js
+++ src/index.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Białystok kamery
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.1
 // @description  Adds a simple panel to switch a camera
 // @author       Łukasz Brzózko
-// @match        http://kamery.szr.bialystok.pl:81/*
+// @match        http://kamery.szr.bialystok.pl:81/kam*
 // @icon         http://szr.bialystok.pl/favicon.ico
 // @resource     openProps https://unpkg.com/open-props@1.5.11/open-props.min.css
 // @resource     openPropsNormalize https://unpkg.com/open-props@1.5.11/normalize.min.css
