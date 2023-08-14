7effecadf782e8baaec250c19a6cf29982abb9ee
Łukasz Brzózko <lukasz9099@gmail.com>
2023-08-14 17:27:30 +0200

    fix: update/download links

--- dist/index.meta.js
+++ dist/index.meta.js
@@ -9,8 +9,8 @@
 // @resource     openProps https://unpkg.com/open-props@1.5.11/open-props.min.css
 // @resource     openPropsNormalize https://unpkg.com/open-props@1.5.11/normalize.min.css
 // @resource     openPropsButtons https://unpkg.com/open-props@1.5.11/buttons.min.css
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/bialystok-kamery/main/index.meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/bialystok-kamery/main/index.user.js
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/bialystok-kamery/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/bialystok-kamery/main/dist/index.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
